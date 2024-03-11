bf173e464ae8e9b48e4153d75be4b85f9cc2efc9
Luissa <ulieluissa@gmail.com>
2024-03-11 12:37:03 +0700

    fix: fix build suspense

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -2,20 +2,23 @@ import Image from 'next/image';
 import styles from './home.module.css';
 import logo from './assets/logo.svg';
 import { SearchComponent } from '../result/ui';
+import { Suspense } from 'react';
 
 export default function Home() {
   return (
-    <div className={styles.bg}>
-      <Image
-        src={logo}
-        width={72.2}
-        height={83.8}
-        alt="logo image"
-        className={styles.logo}
-      />
-      <div className={styles.bottom}>
-        <SearchComponent />
+    <Suspense>
+      <div className={styles.bg}>
+        <Image
+          src={logo}
+          width={72.2}
+          height={83.8}
+          alt="logo image"
+          className={styles.logo}
+        />
+        <div className={styles.bottom}>
+          <SearchComponent />
+        </div>
       </div>
-    </div>
+    </Suspense>
   );
 }
